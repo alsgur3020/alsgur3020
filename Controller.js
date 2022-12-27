e7e34e7f4076c530094e91e68a3de0517d14b20b
alsgur3020 <74518384+alsgur3020@users.noreply.github.com>
2022-12-27 14:22:03 +0900

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -3,7 +3,7 @@ public class TestController {
     @GetMapping("/test")
     public String test(){
         String testStr = "Hi~~";
-        System.out.println(testStr);https://github.com/alsgur3020/alsgur3020
+        System.out.println(testStr);
         return testStr;
     }
 }
